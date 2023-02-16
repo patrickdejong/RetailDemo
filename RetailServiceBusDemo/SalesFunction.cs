5e321c95d907fa1d4ae75895bb5822a5514b34b0
Patrick de Jong <p.jong@caesar.nl>
2023-02-16 11:53:32 +0100

    Add deploy SalesFunctionApp

--- RetailServiceBusDemo/SalesFunction.cs
+++ RetailServiceBusDemo/SalesFunction.cs
@@ -4,18 +4,13 @@ using DataLibrary.Events;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Text.Json.Nodes;
 
-namespace RetailServiceBusDemo
-{
-    public class SalesFunction
-    {
+namespace RetailServiceBusDemo {
+    public class SalesFunction {
         [FunctionName("SalesFunction")]
         [return: ServiceBus("Orders", Connection = "ServiceBusConnectionString")]
-        public static ServiceBusMessage Run([ServiceBusTrigger("SalesQueue", Connection = "ServiceBusConnectionString")] string myQueueItem, ILogger log)
-        {
+        public static ServiceBusMessage Run([ServiceBusTrigger("SalesQueue", Connection = "ServiceBusConnectionString")] string myQueueItem, ILogger log) {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
             PlaceOrder order = JsonConvert.DeserializeObject<PlaceOrder>(myQueueItem);
 
@@ -25,7 +20,7 @@ namespace RetailServiceBusDemo
             };
 
             ServiceBusMessage message = new ServiceBusMessage(JsonConvert.SerializeObject(orderPlaced));
-            message.ApplicationProperties.Add("Type","OrderPlaced");
+            message.ApplicationProperties.Add("Type", "OrderPlaced");
             message.Body = new BinaryData(JsonConvert.SerializeObject(orderPlaced));
             return message;
         }
